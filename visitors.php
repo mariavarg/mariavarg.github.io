76f444159b205bfe9f575fda4f79abd119953a32
mariavarg <75952007+mariavarg@users.noreply.github.com>
2021-11-14 20:38:21 +0200

    Update visitors.php

--- visitors.php
+++ visitors.php
@@ -25,6 +25,9 @@ mycursor = mydb.cursor()
 
 mycursor.execute("CREATE TABLE visitors (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")?>
 
+<?php
+ $Subject = $_POST["Subject"]
+
 
 
 
