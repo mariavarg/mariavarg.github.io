d6aa5d51de1e782a989bf5103ecd6cab264cc7f8
mariavarg <75952007+mariavarg@users.noreply.github.com>
2021-11-14 22:19:41 +0200

    Update visitors.php

--- visitors.php
+++ visitors.php
@@ -26,9 +26,9 @@ mycursor = mydb.cursor()
 mycursor.execute("CREATE TABLE visitors (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")?>
 
 <?php
- $Subject = $_POST["Subject"]
- $Name = $_POST["Name"]
- $E-mail = $_POST["E-mail"]?>
+ $Subject = $_GET["Subject"]
+ $Name = $_GET["Name"]
+ $E-mail = $_GET["E-mail"]?>
 
 if ((mysqli_affected_rows ($connect) > 0){
      echo "<p>Thanks for your visit</p>";
@@ -36,7 +36,7 @@ if ((mysqli_affected_rows ($connect) > 0){
 else (
      echo "<p>Visitor not added</p>";
      echo mysqli_error($connect);
-)<?>
+)?>
 
 
 
