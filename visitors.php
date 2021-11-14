84d841df0d64d37f1668aa2d6af5c3df52f35806
mariavarg <75952007+mariavarg@users.noreply.github.com>
2021-11-14 20:32:26 +0200

    Update visitors.php

--- visitors.php
+++ visitors.php
@@ -23,6 +23,8 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
-mycursor.execute("CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")?>
+mycursor.execute("CREATE TABLE visitors (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")?>
+
+
 
 
