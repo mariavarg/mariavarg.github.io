030ec2f670c50d3a95b814091cfeca917a5b93db
mariavarg <75952007+mariavarg@users.noreply.github.com>
2021-11-14 21:23:54 +0200

    Update voters.php

--- voters.php
+++ voters.php
@@ -1,1 +1,44 @@
+ <?php
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="myusername",
+  password="mypassword"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE DATABASE myvotersdatabase")?>
+        
+        <?php
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  password="yourpassword",
+  database="myvotersdatabase"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE TABLE visitors (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")?>
+
+<?php
+ $Email = $_POST["Email"]?>
+ $Genre = $_POST["Genre"]
+ $Vote = $_POST["Vote"]
+ 
+if ((mysqli_affected_rows ($connect) > 0){
+     echo "<p>Thanks for your visit</p>";
+     echo "<a href="index.php">Go back</a>;}
+else (
+     echo "<p>Visitor not added</p>";
+     echo mysqli_error($connect);
+)<?>
+
+
+
+
 
