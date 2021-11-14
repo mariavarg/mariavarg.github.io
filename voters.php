9a2998c3e6c2468dfeb54477a7c62db4c9815a2d
mariavarg <75952007+mariavarg@users.noreply.github.com>
2021-11-14 21:28:59 +0200

    Update voters.php

--- voters.php
+++ voters.php
@@ -26,15 +26,15 @@ mycursor = mydb.cursor()
 mycursor.execute("CREATE TABLE visitors (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")?>
 
 <?php
- $Email = $_POST["Email"]
- $Genre = $_POST["Genre"]
- $Vote = $_POST["Vote"]?>
+ $Email = $_POST["Email"];
+ $Genre = $_POST["Genre"];
+ $Vote = $_POST["Vote"];
  
 if ((mysqli_affected_rows ($connect) > 0){
      echo "<p>Thanks for your vote</p>";
      echo "<a href="index.php">Go back</a>;}
 else (
-     echo "<p>Voter not added</p>";
+     echo "Voter not added";
      echo mysqli_error($connect);
 )
 
