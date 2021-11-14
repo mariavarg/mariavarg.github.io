dcb85ed0cb22dda69b6380fb535e615aed1365a4
mariavarg <75952007+mariavarg@users.noreply.github.com>
2021-11-14 23:13:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $txtEmail = $_POST['txtEmail'];
 
 
 // database insert SQL code
-$sql = "INSERT INTO `tbl_contact` (`Id`, `fldName`, `fldEmail`, `fldPhone`, `fldMessage`) VALUES ('0', '$txtName', '$txtEmail', '$txtPhone', '$txtMessage')";
+$sql = "INSERT INTO `tbl_contact` (`Id`, `fldName`, `fldEmail`,'fldMessage`) VALUES ('0', '$txtName', '$txtEmail', '$txtPhone', '$txtMessage')";
 
 // insert in database 
 $rs = mysqli_query($con, $sql);
@@ -25,3 +25,46 @@ if($rs)
 }
 
 ?>
+
+--
+-- Database: `db_contact`
+--
+
+CREATE DATABASE IF NOT EXISTS `db_contact` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `db_contact`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_contact`
+--
+
+DROP TABLE IF EXISTS `tbl_contact`;
+CREATE TABLE `tbl_contact` (
+  `id` int(11) NOT NULL,
+  `fldSubject` text NOT NULL
+  `fldName` varchar(50) NOT NULL,
+  `fldEmail` varchar(150) NOT NULL,
+  
+  
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_contact`
+--
+ALTER TABLE `tbl_contact`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_contact`
+--
+ALTER TABLE `tbl_contact`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
