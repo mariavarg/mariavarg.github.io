9c2a0da929c76fae0cd946bc355c3caaa07c6bf2
mariavarg <75952007+mariavarg@users.noreply.github.com>
2021-12-06 17:39:27 +0200

    Update database.php

--- contact/database.php
+++ contact/database.php
@@ -4,8 +4,8 @@ $password = ' ';
 $inbox = imap_open(https://mail.google.com/mail/u/0/?fbclid=IwAR1pYSsBCX7DTi7cepg-xY1RQTr5hDZtOGDfGkrIot5EugAOSX2eELw597Q#inbox,$root@localhost,$password);
 $emails = imap_search($inbox,'ALL');
 foreach($emails as $e){
-    $overview = imap_fetch_overview($inbox,$e,0);
-    $Subject = imap_fetchbody($inbox,$e,2);
+    $overview = imap_fetch_overview($https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox,$e,0);
+    $Subject = imap_fetchbody($https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox,$e,2);
     // the body of the message is in $Subject
     $details = $overview[0];
     
