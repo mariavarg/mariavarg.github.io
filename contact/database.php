e629c85c8e76dd1c62341957dceb27bb239deb01
mariavarg <75952007+mariavarg@users.noreply.github.com>
2021-12-06 18:05:04 +0200

    Update database.php

--- contact/database.php
+++ contact/database.php
@@ -1,8 +1,8 @@
 $hostname = '{https://mail.google.com/mail/u/0/?fbclid=IwAR1pYSsBCX7DTi7cepg-xY1RQTr5hDZtOGDfGkrIot5EugAOSX2eELw597Q#inbox:993/imap/ssl}INBOX';
 $username = ' root@localhost';
 $password = ' ';
-$inbox = imap_open(https://mail.google.com/mail/u/0/?fbclid=IwAR1pYSsBCX7DTi7cepg-xY1RQTr5hDZtOGDfGkrIot5EugAOSX2eELw597Q#inbox,$root@localhost,$password);
-$emails = imap_search($inbox,'ALL');
+$inbox = imap_open(https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox,$root@localhost,$password);
+$emails = imap_search($https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox,'ALL');
 foreach($emails as $e){
     $overview = imap_fetch_overview($https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox,$e,0);
     $Subject = imap_fetchbody($https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox,$e,2);
