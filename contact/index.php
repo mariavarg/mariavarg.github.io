97784a24959a0e9eec2b5c84b64f0aa7ed51e152
mariavarg <75952007+mariavarg@users.noreply.github.com>
2021-12-07 12:47:42 +0200

    Update index.php

--- contact/index.php
+++ contact/index.php
@@ -35,12 +35,6 @@ body, h1 {
 
     </form>
   
-<?php>
-  $sql = "INSERT INTO visitors (visitor_id, visitor_subject, visitor_name, visitor_email) VALUES ('0', '$txtSubject', '$txtName', '$txtE-mail');";
-  mysqli_query($conn, $sql);
-?>
-
-
 <?php
 
 include_once "includes/dbh.inc.php";
