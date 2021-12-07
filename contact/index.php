2b4c973a5f48452d6f45c6fce193da371637ab41
mariavarg <75952007+mariavarg@users.noreply.github.com>
2021-12-07 12:59:41 +0200

    Update index.php

--- contact/index.php
+++ contact/index.php
@@ -37,13 +37,10 @@ body, h1 {
   
 <?php
 
-include_once "includes/dbh.inc.php";
-
-?>
-
 $dbServername = "localhost";
 $dbUsername = "root";
 $dbPassword = "";
 $dbName = "loginsystem";
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+?>    
