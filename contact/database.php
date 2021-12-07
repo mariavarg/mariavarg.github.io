c9a88391869276ad2499600b7a50771b85456a12
mariavarg <75952007+mariavarg@users.noreply.github.com>
2021-12-07 11:53:33 +0200

    Update database.php

--- contact/database.php
+++ contact/database.php
@@ -1,14 +1,6 @@
-$hostname = '{localhost}INBOX';
-$username = ' root';
-$password = ' ';
-$inbox = imap_open(https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox,$root@localhost,$password);
-$emails = imap_search($https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox,'ALL');
-foreach($emails as $e){
-    $overview = imap_fetch_overview($https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox,$e,0);
-    $Subject = imap_fetchbody($https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox,$e,2);
-    // the body of the message is in $Subject
-    $details = $overview[0];
-    
-}
-$cfg['Console']['Mode'] = 'show';
-
+CREATE TABLE visitors (
+visitor_id int(11) AUTO_INCREMENT PRIMARY KEY not null,
+visitor_subject text
+visitor_name varchar(256) not null,
+visitor_email varchar(256) not null
+);
