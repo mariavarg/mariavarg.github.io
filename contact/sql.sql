841c91e5619261be399adf3a75883dd590e53417
mariavarg <75952007+mariavarg@users.noreply.github.com>
2021-12-07 13:27:07 +0200

    Update sql.sql

--- contact/sql.sql
+++ contact/sql.sql
@@ -1,40 +1,9 @@
---
--- Database: `db_contact`
---
+<?php
 
-CREATE DATABASE IF NOT EXISTS `db_contact` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `db_contact`;
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "loginsystem";
 
---
--- 
---
-
-DROP TABLE IF EXISTS `tbl_contact`;
-CREATE TABLE `tbl_contact` (
-  `id` int(11) NOT NULL,
-  `fldSubject` text NOT NULL
-  `fldName` varchar(50) NOT NULL,
-  `fldEmail` varchar(150) NOT NULL,
-  
-  
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- 
---
-
---
--- Indexes for table `tbl_contact`
---
-ALTER TABLE `tbl_contact`
- ADD PRIMARY KEY (`id`);
-
---
--- 
---
-
---
--- AUTO_INCREMENT for table `tbl_contact`
---
-ALTER TABLE `tbl_contact`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+?>    
