f869c29f096fc7f63ac498054d55064343bb08a8
mariavarg <75952007+mariavarg@users.noreply.github.com>
2021-12-07 12:51:52 +0200

    Delete postemail.php

--- contact/postemail.php
+++
@@ -1,8 +0,0 @@
-<?php
-mysql_select_db("db_contact", $connection);
-$sql="INSERT INTO Subscribers (EmailAddress) VALUES ('$_POST[emailaddress]')";
-
-if (!mysql_query($sql,$connection)) { die('Error: ' . mysql_error()); }
-
-mysql_close($connection); ?>
-
