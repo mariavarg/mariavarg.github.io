7f95e06d7cc7648dbd9dd88e4bfd156416cf784b
mariavarg <75952007+mariavarg@users.noreply.github.com>
2021-12-07 12:46:51 +0200

    Update submit.inc.php

--- contact/submit.inc.php
+++ contact/submit.inc.php
@@ -1,1 +1,4 @@
-
+<?php>
+  $sql = "INSERT INTO visitors (visitor_id, visitor_subject, visitor_name, visitor_email) VALUES ('0', '$txtSubject', '$txtName', '$txtE-mail');";
+  mysqli_query($conn, $sql);
+?>
