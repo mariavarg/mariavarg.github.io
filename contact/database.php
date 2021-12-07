dc9f3689b89be486b35946d52d6ec68cc46e328b
mariavarg <75952007+mariavarg@users.noreply.github.com>
2021-12-07 13:10:33 +0200

    Delete database.php

--- contact/database.php
+++
@@ -1,8 +0,0 @@
-CREATE TABLE visitors (
-visitor_id int(11) AUTO_INCREMENT PRIMARY KEY not null,
-visitor_subject text
-visitor_name varchar(256) not null,
-visitor_email varchar(256) not null
-);
-
-INSERT INTO visitors (visitor_id, visitor_subject, visitor_name, visitor_email) VALUES ('0', '$txtSubject', '$txtName', '$txtE-mail')";
