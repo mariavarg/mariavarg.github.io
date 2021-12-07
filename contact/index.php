83b1364399bd074ba007034fd333d6b78142155b
mariavarg <75952007+mariavarg@users.noreply.github.com>
2021-12-07 12:23:23 +0200

    Update index.php

--- contact/index.php
+++ contact/index.php
@@ -7,7 +7,7 @@
   
 <?php>
   $sql = "INSERT INTO visitors (visitor_id, visitor_subject, visitor_name, visitor_email) VALUES ('0', '$txtSubject', '$txtName', '$txtE-mail');";
-  $result = mysqli_query($conn, $sql);
+  mysqli_query($conn, $sql);
 ?>
 
 
