17dd272ea1448f5dab9eb80c28640533eac81e4c
mariavarg <75952007+mariavarg@users.noreply.github.com>
2021-12-07 16:22:11 +0200

    Update sql.sql

--- contact/sql.sql
+++ contact/sql.sql
@@ -6,4 +6,25 @@ $dbPassword = "";
 $dbName = "loginsystem";
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
-?>    
+?>  
+
+<?php    
+    
+while($row = mysql_fetch_object($result)){    
+     
+?>  
+    <tr>  
+        <td>  
+            <?php echo $row->id;?>  
+        </td>  
+        <td>  
+            <?php echo $row->subject;?>  
+        </td>  
+        <td>  
+            <?php echo $row->name;?>  
+        </td>
+		<td>  
+            <?php echo $row->mail;?>  
+        </td>          
+        <tr>  
+            <? php } ?>  
