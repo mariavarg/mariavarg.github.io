b6b872bfe9c643b8e9939f7f75927bac58a1a491
mariavarg <75952007+mariavarg@users.noreply.github.com>
2021-12-07 13:03:03 +0200

    Update submit.inc.php

--- contact/submit.inc.php
+++ contact/submit.inc.php
@@ -3,7 +3,7 @@
 include_once "includes/dbh.inc.php";
 
 $subject = $_POST["subject"];
-$subject = $_POST["subject"];
+$name = $_POST["name"];
 $subject = $_POST["subject"];
   
   $sql = "INSERT INTO visitors (visitor_id, visitor_subject, visitor_name, visitor_email) VALUES ('0', '$txtSubject', '$txtName', '$txtE-mail');";
