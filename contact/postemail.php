21a1da47a1a3974de221ad7bc4025c053cccdd68
mariavarg <75952007+mariavarg@users.noreply.github.com>
2021-12-07 09:55:48 +0200

    Update postemail.php

--- contact/postemail.php
+++ contact/postemail.php
@@ -1,13 +1,4 @@
 <?php
-// Create the Subscribers table tlb_contact("db_contact", $connection); $sql = "CREATE TABLE Subscribers ( EmailAddress varchar(35) )";
-
-// Execute query mysql_query($sql,$connection);
-
-mysql_close($connection); ?>
-
-
-
-<?php
 mysql_select_db("db_contact", $connection);
 $sql="INSERT INTO Subscribers (EmailAddress) VALUES ('$_POST[emailaddress]')";
 
