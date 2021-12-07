66614edb3ebb622cfa8ded5c29708c11216cbf39
mariavarg <75952007+mariavarg@users.noreply.github.com>
2021-12-07 16:42:46 +0200

    Create sql.sql

---
+++ contact/sql.sql
@@ -0,0 +1,30 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "loginsystem";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+?>  
+
+<?php    
+    
+while($row = mysql_fetch_object($result)){    
+     
+?>  
+    <tr>  
+        <td>  
+            <?php echo $row->id;?>  
+        </td>  
+        <td>  
+            <?php echo $row->subject;?>  
+        </td>  
+        <td>  
+            <?php echo $row->name;?>  
+        </td>
+		<td>  
+            <?php echo $row->email;?>  
+        </td>          
+        <tr>  
+            <? php } ?>  
