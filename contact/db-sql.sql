bac0ca9d038e1a0a7f92588aa72ef2f7dde3b6ea
mariavarg <75952007+mariavarg@users.noreply.github.com>
2021-12-10 18:20:03 +0200

    Update db-sql.sql

--- contact/db-sql.sql
+++ contact/db-sql.sql
@@ -15,10 +15,9 @@ USE `db_contact`;
 DROP TABLE IF EXISTS `tbl_contact`;
 CREATE TABLE `tbl_contact` (
   `id` int(11) NOT NULL,
-  `fldName` varchar(50) NOT NULL,
-  `fldEmail` varchar(150) NOT NULL,
-  `fldPhone` varchar(15) NOT NULL,
-  `fldMessage` text NOT NULL
+  `fldName` varchar(256) NOT NULL,
+  `fldEmail` varchar(256) NOT NULL,
+  `fldSubject` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
