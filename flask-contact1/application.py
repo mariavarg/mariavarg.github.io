d904bc9401210e686864955a4b500cc4f976c62b
mariavarg <75952007+mariavarg@users.noreply.github.com>
2021-12-10 14:16:28 +0200

    Update application.py

--- flask-contact1/application.py
+++ flask-contact1/application.py
@@ -12,6 +12,10 @@ app.config["MAIL_SERVER"] = smtp.gmail.com
 app.config["MAIL_USE_TLS"] = True
 app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
 mail = Mail(app)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 
 @app.route("/")
 def index():
