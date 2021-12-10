5c3dd84ecc2a3996060ad1f5bdceed3f23ca57a5
mariavarg <75952007+mariavarg@users.noreply.github.com>
2021-12-10 16:04:51 +0200

    Create application.py

---
+++ application.py
@@ -0,0 +1,61 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, url_for(), redirect(), session(), render_template(), request(), jsonify(), flash()
+from flask_mail import Mail, Message
+
+app = Flask(__name__)
+app.config["MAIL_DEFAULT_SENDER"] = os.getenv("MAIL_DEFAULT_SENDER")
+app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_SERVER"] = smtp.gmail.com
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
+mail = Mail(app)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+@app.route("/")
+def index():
+  return rendrer_template("index.html", name=request.args.get("name"))
+
+export FLASK_APP=application.py
+export FLASK_DEBUG=1
+flask run
+
+db = SQL("sqlite:///templates.db")
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+  if not request.form.get("username")
+    return apology("must provide username")
+
+  def register():
+    if not email:
+        return render_template("error.html", message="Missing email")
+         = request.form.get(" ")
+    if  not in
+        return render_template("error.html", message="Invalid E-mail")
+  
+  if request method== "POST":
+    return render_template("Success.html")
+  else
+    return render_template("contactme.html")
+  
+ db.execute("INSERT INTO  (name, ) VALUES(?, ?)", name,
+
+        message = Message("Thanks for visiting", recipients=[email])
+        mail.send(message)
+
+    return redirect("/")
+
+@app.route("/")
+def ():
+     = db.execute("SELECT * FROM ")
+    return render_template(" ", )
+
+ 
+  
+  
