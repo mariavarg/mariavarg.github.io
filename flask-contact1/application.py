015fbba0300e6b83633b87fed33477431b827853
mariavarg <75952007+mariavarg@users.noreply.github.com>
2021-12-10 13:33:48 +0200

    Update application.py

--- flask-contact1/application.py
+++ flask-contact1/application.py
@@ -1,13 +1,27 @@
+import os
+
+from cs50 import SQL
 from flask import Flask, url_for(), redirect(), session(), render_template(), request(), jsonify()
+from flask_mail import Mail, Message
+
 app = Flask(__name__)
+app.config["MAIL_DEFAULT_SENDER"] = os.getenv("MAIL_DEFAULT_SENDER")
+app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_SERVER"] = smtp.gmail.com
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
+mail = Mail(app)
 
 @app.route("/")
 def index():
-  return rendrer_template"index.html"
+  return rendrer_template("index.html", name=request.args.get("name"))
 
 export FLASK_APP=application.py
 export FLASK_DEBUG=1
 flask run
+
+db = SQL("sqlite:///templates.db")
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
@@ -18,5 +32,19 @@ def login():
     return render_template"Success.html"
   else
     return render_template"contactme.html"
+  
+ db.execute("INSERT INTO  (name, ) VALUES(?, ?)", name,
+
+        message = Message("Thanks for visiting", recipients=[email])
+        mail.send(message)
+
+    return redirect("/")
+
+@app.route("/")
+def ():
+     = db.execute("SELECT * FROM ")
+    return render_template(" ", )
+
+ 
   
   
