92f3ea8b75303bfebad0a281d5d9e6b291a314e5
mariavarg <75952007+mariavarg@users.noreply.github.com>
2022-05-30 16:18:04 +0300

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,12 @@
+<?php
+
+<p><span class="error">* required field</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name" value="<?php echo $name;?>">
+  <span class="error">* <?php echo $nameErr;?></span>
+  <br><br>
+  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+  <span class="error">* <?php echo $emailErr;?></span>
+  <br><br>
+
+?>
