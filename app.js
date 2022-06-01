59ebe0e5e9a784cfdbbe256752e7facc567f5f62
mariavarg <75952007+mariavarg@users.noreply.github.com>
2022-06-02 00:18:21 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+
+const app = express();
+const port =process.env.PORT || ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{
+  "name": "express-file-uploads",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "start": "nodemode app.js"
+  "author": "",
+  "license": "ISC",
+  "dependencies"":{
+     ""mysql": "2.0.0";
+  }   
+    devDependencies": {
+    "nodemon": "^2.0.16"
+  }
+}
+
