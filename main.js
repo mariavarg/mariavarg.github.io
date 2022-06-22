491b40da40e332cb554c3ef7149917b83f9a2de0
mariavarg <75952007+mariavarg@users.noreply.github.com>
2022-06-22 11:20:40 +0300

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Let 's get Creative</title>
-    <link rel='stylesheet' type='text/css' media='screen' href='css/styles.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='styles.css'>
     <title>Javascript</title> 
         
 </div>
