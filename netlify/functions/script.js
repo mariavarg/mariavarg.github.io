fc9a4dd1fafcbba5a5370dcac5ca286fc1c09e1f
mariavarg <75952007+mariavarg@users.noreply.github.com>
2022-07-04 13:07:05 +0300

    Update script.js

--- netlify/functions/script.js
+++ netlify/functions/script.js
@@ -2,6 +2,16 @@
 <div id="display-image"></div>
 
 const image_input = document.querySelector("#image-input");
+image_input.addEventListener("change", function() {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => {
+    const uploaded_image = reader.result;
+    document.querySelector("#display-image").style.backgroundImage = `url(${uploaded_image})`;
+  });
+ 
+
+
+const image_input = document.querySelector("#image-input");
 
         image_input.addEventListener("change", function() {
           const reader = new FileReader();
