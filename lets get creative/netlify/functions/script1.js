8eace295f1b39df8cd7d9542d47c720ba59d85eb
mariavarg <75952007+mariavarg@users.noreply.github.com>
2022-07-05 20:23:40 +0300

    Create script1.js

---
+++ lets get creative/netlify/functions/script1.js
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Let 's get Creative</title>
+    <link rel='stylesheet' type='text/css' media='screen' href='lets get creative/css/styles.css'>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Sofia&display=swap');
+        </style>
+    </head>
+
+<body>
+         
+            <script>
+const quoteText = document.getElementById("quote__text");
+const image_input = document.querySelector("#image_input");
+var uploaded_image = "";
+
+image_input.addEventListener("change", function() {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => {
+    const uploaded_image = reader.result;
+    document.querySelector("#display_image").style.backgroundImage = `url(${uploaded_image})`;
+  });
+  reader.readAsDataURL(this.files[0]);
+});
