5feedea5ecc96860a8296eeef811ae696c573182
mariavarg <75952007+mariavarg@users.noreply.github.com>
2022-07-06 01:24:04 +0300

    Update script.js

--- lets get creative/netlify/functions/script.js
+++ lets get creative/netlify/functions/script.js
@@ -27,10 +27,6 @@ image-input.addEventListener("change", function() {
   reader.readAsDataURL(this.files[0]);
 });
 
-const quoteText = document.getElementById("quote__text");
-const image-input = document.querySelector("#image-input");
-var uploaded-image = "";
-
 function generate() {
     document.getElementById("render").innerHTML = "";
     html2canvas(document.querySelector("#capture")).then(canvas => {
