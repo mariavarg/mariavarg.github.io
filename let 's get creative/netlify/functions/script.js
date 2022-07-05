d44aa950c9a0886d515387e6b32da4c2249b81b3
mariavarg <75952007+mariavarg@users.noreply.github.com>
2022-07-05 17:09:14 +0300

    Update script.js

--- let 's get creative/netlify/functions/script.js
+++ let 's get creative/netlify/functions/script.js
@@ -15,7 +15,7 @@
          
             <script>
 const quoteText = document.getElementById("quote__text");
-const image_input = document.querySelector("#image-input");
+const image_input = document.querySelector("#image_input");
 var uploaded_image = "";
 
 image_input.addEventListener("change", function() {
