9474c3c79a8c36ecac658a7e2bee4c2752684b8c
mariavarg <75952007+mariavarg@users.noreply.github.com>
2022-07-10 11:58:53 +0300

    Update script.js

--- lets get creative/netlify/functions/script.js
+++ lets get creative/netlify/functions/script.js
@@ -14,7 +14,17 @@
 <body>
          
             <script>
+const image_input = document.querySelector("#image-input");
 const quoteText = document.getElementById("quote__text");
+
+image_input.addEventListener("change", function() {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => {
+    const uploaded_image = reader.result;
+    document.querySelector("#display-image").style.backgroundImage = `url(${uploaded_image})`;
+  });
+  reader.readAsDataURL(this.files[0]);
+});
 
 function generate() {
     document.getElementById("render").innerHTML = "";
