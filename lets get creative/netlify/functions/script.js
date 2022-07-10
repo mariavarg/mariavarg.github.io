9ca5c82128d5df6a6b4579451ed765701c0fd9ef
mariavarg <75952007+mariavarg@users.noreply.github.com>
2022-07-10 16:34:33 +0300

    Update script.js

--- lets get creative/netlify/functions/script.js
+++ lets get creative/netlify/functions/script.js
@@ -38,7 +38,7 @@ document.querySelector("textarea").addEventListener('keyup', function(){
         renderCanvas();
     }
     else {
-        quoteText.innerHTML = "Start typing </br>:)"
+        quoteText.innerHTML = "Start typing </br>"
     }
     
 });
