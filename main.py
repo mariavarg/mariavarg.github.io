807b85d99d8a08d2f1a48afe0935762c6995fc06
mariavarg <75952007+mariavarg@users.noreply.github.com>
2022-12-25 08:22:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -155,9 +155,27 @@ if __name__ == '__main__':
                 speak('Goodbye')
                 break
                 
-                <html>
-                <body>
-                
+                <!DOCTYPE html>
+
+<meta name="viewport" content="width=device-width-10, initial-scale=1.0">
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+        <link href="styles1.css" rel="stylesheet">
+        <title>My Website</title>
+    </head>
+<head>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia">
+<style>
+body, h2, h4 {
+  font-family: "Sofia", sans-serif;
+}
+</style>
+</head>
+
+<body>
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
 <ul class="navbar-nav">
 <li class="nav-item">
