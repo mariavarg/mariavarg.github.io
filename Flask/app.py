7d30b2eac5ce7c267fea6eafd2aed864c48189eb
mariavarg <75952007+mariavarg@users.noreply.github.com>
2023-06-18 22:28:52 +0300

    Add files via upload

---
+++ Flask/app.py
@@ -0,0 +1,32 @@
+from PIL import Image, ImageDraw, ImageFont
+
+def process_image(image_file):
+    # Open the image
+    image = Image.open(image_file)
+
+    # Create an ImageDraw object
+    draw = ImageDraw.Draw(image)
+
+    # Choose a font and font size
+    font = ImageFont.truetype('
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia', 36)
+
+    # Get the size of the text
+    text = 'My Poster'
+    text_width, text_height = draw.textsize(text, font=font)
+
+    # Calculate the x and y coordinates for the text
+    x = (image.width - text_width) / 2
+    y = (image.height - text_height) / 2
+
+    # Set the text color
+    color = (0, 0, 0)
+
+    # Draw the text on the image
+    draw.text((x, y), text, font=font, fill=color)
+
+    # Save the image
+    output_image_path = '/workspaces/75952007/Flask/static/images/poster.png'
+    image.save(output_image_path)
+
+    return output_image_path
