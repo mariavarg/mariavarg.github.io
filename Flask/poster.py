6bdd2a41b41e41a9d9ad92b96b558d324bbae1c2
mariavarg <75952007+mariavarg@users.noreply.github.com>
2023-06-18 07:13:43 +0300

    Update poster.py

--- Flask/poster.py
+++ Flask/poster.py
@@ -29,3 +29,35 @@ def process_image(image_file):
     image.save(output_image_path)
 
     return output_image_path
+
+
+ <nav class="navbar navbar-expand-bg bg-dark navbar-dark">
+<ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="index.html">HOME!</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="contactme.html">CONTACT</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="about.html">ABOUT</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="Flask/poster.py">NEXT</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="page20.html">PREVIOUS</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="javascript:history.go(-1)">BACK</a>
+    </li>
+   </ul>
+
+</nav>
+        
+        </body>
+
+<audio controls autoplay>
+  <source src="Metallica - Fade to Black (Official Video).m4a">
+
+</audio>
