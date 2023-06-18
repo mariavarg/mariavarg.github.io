0887d769f5b1564665ef35f3fb6e4cb6bfc9e9f4
mariavarg <75952007+mariavarg@users.noreply.github.com>
2023-06-18 07:20:51 +0300

    Update poster.py

--- Flask/poster.py
+++ Flask/poster.py
@@ -30,6 +30,31 @@ def process_image(image_file):
 
     return output_image_path
 
+<!DOCTYPE html>
+
+<meta name="viewport" content="width=device-width-10, initial-scale=1.0">
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+        <link href="styles1.css" rel="stylesheet">
+        <meta name="google-site-verification" content="lx9Buu8regrl5IfmZntGJ-oDlfmAMPucGl-y4_8W9RE" />
+        <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@latest"> </script>
+        <!-- Make sure your page supports utf-8 characterset. -->
+
+      <style>
+.content {
+  max-width: 500px;
+  margin: auto;
+}</style>
+            
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+    <div class="content">
+<!-- Page content -->
+      
+    <body>
 
  <nav class="navbar navbar-expand-bg bg-dark navbar-dark">
 <ul class="navbar-nav">
@@ -61,3 +86,5 @@ def process_image(image_file):
   <source src="Metallica - Fade to Black (Official Video).m4a">
 
 </audio>
+
+</html>
