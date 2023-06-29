42d27a9a2b9d9a4abaf52d9cc8debd171fed1d31
mariavarg <75952007+mariavarg@users.noreply.github.com>
2023-06-29 07:06:23 +0300

    Add files via upload

---
+++ Tetris.io-main/main.py
@@ -0,0 +1,90 @@
+import pygame
+import random
+import pygame.mixer
+
+# Rest of the code...
+
+def main():
+    global grid
+
+    pygame.mixer.music.load("06-Kashmir.mp3")  # Replace "path_to_kashmir.mp3" with the actual file path of the "Kashmir" song
+    pygame.mixer.music.play(-1)  # -1 indicates that the music should loop indefinitely
+
+    locked_positions = {}  # (x,y):(255,0,0)
+    grid = create_grid(locked_positions)
+
+    change_piece = False
+    run = True
+    current_piece = get_shape()
+    next_piece = get_shape()
+    clock = pygame.time.Clock()
+    fall_time = 0
+    level_time = 0
+    fall_speed = 0.27
+    score = 0
+    
+    while run:
+        # Rest of the game loop
+          grid = create_grid(locked_positions)
+        fall_time += clock.get_rawtime()
+        level_time += clock.get_rawtime()
+        clock.tick()
+
+        if level_time/1000 > 4:
+            level_time = 0
+            if fall_speed > 0.15:
+                fall_speed -= 0.005
+            
+
+        # PIECE FALLING CODE
+        if fall_time/1000 >= fall_speed:
+            fall_time = 0
+            current_piece.y += 1
+            if not (valid_space(current_piece, grid)) and current_piece.y > 0:
+                current_piece.y -= 1
+                change_piece = True
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.display.quit()
+                quit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    current_piece.x -= 1
+                    if not valid_space(current_piece, grid):
+                        current_piece.x += 1
+
+                elif event.key == pygame.K_RIGHT:
+                    current_piece.x += 1
+                    if not valid_space(current_piece, grid):
+                        current_piece.x -= 1
+                elif event.key == pygame.K_UP:
+                    # rotate shape
+                    current_piece.rotation = (current_piece.rotation + 1) % len(current_piece.shape)
+                    if not valid_space(current_piece, grid):
+                        current_piece.rotation = (current_piece.rotation - 1) % len(current_piece.shape)
+
+                if event.key == pygame.K_DOWN:
+                    # move shape down
+                    current_piece.y += 1
+                    if not valid_space(current_piece, grid):
+                        current_piece.y -= 1
+
+        shape_pos = convert_shape_format(current_piece)
+
+        # add piece to the grid for drawing
+        for i in range(len(shape_pos)):
+            x, y = shape_pos[i]
+            if y > -1:
+                grid[y][x] = current_piece.color
+
+        # IF PIECE HIT GROUND
+        if change_piece:
+            for pos in shape_pos:
+                p = (pos[0], pos[1])
+                locked_positions[p] = current_piece.color
+            current_piece = next_piece
+
+    pygame.mixer.music.stop()
