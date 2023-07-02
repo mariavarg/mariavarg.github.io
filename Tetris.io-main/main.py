6e4f98ed2b5cbf462a565a7bb61388edad768d2b
mariavarg <75952007+mariavarg@users.noreply.github.com>
2023-07-02 12:07:50 +0300

    Update main.py

--- Tetris.io-main/main.py
+++ Tetris.io-main/main.py
@@ -24,7 +24,7 @@ represented in order by 0 - 6
 pygame.font.init()
 
 # GLOBALS VARS
-s_width = 450px
+s_width = 450
 s_height = 700
 play_width = 300  # meaning 300 // 10 = 30 width per block
 play_height = 600  # meaning 600 // 20 = 20 height per block
