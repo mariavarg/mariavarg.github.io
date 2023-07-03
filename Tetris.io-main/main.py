2c6dcb8e66c2d4f1677914cf7a654bb4eb25863f
mariavarg <75952007+mariavarg@users.noreply.github.com>
2023-07-03 18:10:21 +0300

    Update main.py

--- Tetris.io-main/main.py
+++ Tetris.io-main/main.py
@@ -1,10 +1,8 @@
-# pygame setup
+import pygame
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
-import pygame
-
 import random
 import pygame.mixer
 
