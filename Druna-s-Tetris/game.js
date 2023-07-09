916755b5a754fc2a917fd166aeb210bba23c4959
mariavarg <75952007+mariavarg@users.noreply.github.com>
2023-07-09 22:07:27 +0300

    Update game.js

--- Druna-s-Tetris/game.js
+++ Druna-s-Tetris/game.js
@@ -1,43 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Game Page</title>
-    <!-- Include necessary CSS stylesheets -->
-    <link rel="stylesheet" href="game.css">
-    <link href="https://fonts.googleapis.com/css?family=Sofia" rel="stylesheet">
-    <style>
-        /* Additional styles for the game page */
-        #game-container {
-            max-width: 450px;
-            margin: 0 auto;
-            margin-top: 50px; /* Adjust the top margin as needed */
-        }
-    </style>
-</head>
-<body>
-    <div id="game-container">
-        <!-- Include the game canvas here -->
-        <canvas id="game-canvas"></canvas>
-    </div>
-
-    <!-- Include necessary scripts -->
-    <script src="https://cdn.jsdelivr.net/pyodide/v0.18.1/full/pyodide.js"></script>
-    <script>
-        // Load Pyodide and execute the game code
-        languagePluginLoader.then(() => {
-            pyodide.runPython(`
-                # Import necessary modules and run the game code
-                import pygame
-                import random
-                
-                # Your game code here
-                
-                # Initialize the game
-                pygame.init()
-                # ...
-            `);
-        });
-    </script>
-</body>
-</html>
+// Load Pyodide and execute the game code
+languagePluginLoader.then(() => {
+    pyodide.runPython(`
+        // Import necessary modules and run the game code
+        import pygame
+        import random
+        
+        // Your game code here
+        
+        // Initialize the game
+        pygame.init()
+        // ...
+    `);
+});
