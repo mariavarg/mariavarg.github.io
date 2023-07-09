ccd82f2f94f4d331eb44408e133dc8545279fd11
mariavarg <75952007+mariavarg@users.noreply.github.com>
2023-07-09 03:11:19 +0300

    Update main.py

--- Druna-s-Tetris/main.py
+++ Druna-s-Tetris/main.py
@@ -17,12 +17,189 @@ top_left_y = 50
 top_left_x = (s_width - play_width) // 2
 top_left_y = s_height - play_height
 
-background = pygame.image.load('/front.jpg')
-back = pygame.image.load('/back2.jpg')
-try_again = pygame.image.load('/try.jpg')
+# color
+gray = (119, 118, 110)
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (200, 0, 0)
+green = (0, 200, 0)
+blue = (0, 255, 255)
+bright_red = (255, 0, 0)
+bright_green = (0, 255, 0)
+sea_green = (0, 255, 255)
+orange = (255, 165, 0)
+yellow = (255, 255, 0)
+purple = (128, 0, 128)
+light_red = (255, 0, 0)
 
-music = pygame.mixer.music.load('kasmir.mp3')
-pygame.mixer.music.play(-1)
+shape_colors = [(0, 255, 0), (255, 0, 0), (0, 255, 255), (255, 255, 0), (255, 165, 0), (0, 0, 255), (128, 0, 128)]
+
+# SHAPE FORMATS
+
+S = [['.....',
+      '.....',
+      '..00.',
+      '.00..',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..00.',
+      '...0.',
+      '.....']]
+
+Z = [['.....',
+      '.....',
+      '.00..',
+      '..00.',
+      '.....'],
+     ['.....',
+      '..0..',
+      '.00..',
+      '.0...',
+      '.....']]
+
+I = [['..0..',
+      '..0..',
+      '..0..',
+      '..0..',
+      '.....'],
+     ['.....',
+      '0000.',
+      '.....',
+      '.....',
+      '.....']]
+
+O = [['.....',
+      '.....',
+      '.00..',
+      '.00..',
+      '.....']]
+
+J = [['.....',
+      '.0...',
+      '.000.',
+      '.....',
+      '.....'],
+     ['.....',
+      '..00.',
+      '..0..',
+      '..0..',
+      '.....'],
+     ['.....',
+      '.....',
+      '.000.',
+      '...0.',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..0..',
+      '.00..',
+      '.....']]
+
+L = [['.....',
+      '...0.',
+      '.000.',
+      '.....',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..0..',
+      '..00.',
+      '.....'],
+     ['.....',
+      '.....',
+      '.000.',
+      '.0...',
+      '.....'],
+     ['.....',
+      '.00..',
+      '..0..',
+      '..0..',
+      '.....']]
+
+T = [['.....',
+      '..0..',
+      '.000.',
+      '.....',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..00.',
+      '..0..',
+      '.....'],
+     ['.....',
+      '.....',
+      '.000.',
+      '..0..',
+      '.....'],
+     ['.....',
+      '..0..',
+      '.00..',
+      '..0..',
+      '.....']]
+
+shapes = [S, Z, I, O, J, L, T]
+shape_colors = [(0, 255, 0), (255, 0, 0), (0, 255, 255), (255, 255, 0), (255, 165, 0), (0, 0, 255), (128, 0, 128)]
+# index 0 - 6 represent shape
+
+
+class Piece:
+    def __init__(self, x, y, shape):
+        self.x = x
+        self.y = y
+        self.shape = shape
+        self.color = shape_colors[shapes.index(shape)]
+        self.rotation = 0
+
+
+def create_grid(locked_positions={}):
+    grid = [[(black) for _ in range(10)] for _ in range(20)]
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if (j, i) in locked_positions:
+                c = locked_positions[(j, i)]
+                grid[i][j] = c
+    return grid
+
+
+def convert_shape_format(shape):
+    positions = []
+    format = shape.shape[shape.rotation % len(shape.shape)]
+
+    for i, line in enumerate(format):
+        row = list(line)
+        for j, column in enumerate(row):
+            if column == '0':
+                positions.append((shape.x + j, shape.y + i))
+
+    for i, pos in enumerate(positions):
+        positions[i] = (pos[0] - 2, pos[1] - 4)
+
+    return positions
+
+
+def valid_space(shape, grid):
+    accepted_pos = [[(j, i) for j in range(10) if grid[i][j] == (black)] for i in range(20)]
+Sorry, but I can't execute Python code that depends on external libraries. I can only provide guidance and help with the code. If you encounter any specific issues or errors, please let me know and I'll be happy to assist you.
+import pygame
+import random
+
+pygame.font.init()
+pygame.init()
+
+# GLOBALS VARS
+s_width = 450
+s_height = 700
+play_width = 300  # meaning 300 // 10 = 30 width per block
+play_height = 600  # meaning 600 // 20 = 30 height per block
+block_size = play_width // 10
+
+top_left_x = (s_width - play_width) // 2
+top_left_y = 50
+
+top_left_x = (s_width - play_width) // 2
+top_left_y = s_height - play_height
 
 # color
 gray = (119, 118, 110)
@@ -150,7 +327,7 @@ shape_colors = [(0, 255, 0), (255, 0, 0), (0, 255, 255), (255, 255, 0), (255, 16
 # index 0 - 6 represent shape
 
 
-class Piece(object):  # *
+class Piece:
     def __init__(self, x, y, shape):
         self.x = x
         self.y = y
@@ -159,61 +336,216 @@ class Piece(object):  # *
         self.rotation = 0
 
 
-def game_intro():
-    gameintro = False
-    while not gameintro:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                gameintro = True
-                game_over = True
+def create_grid(locked_positions={}):
+    grid = [[(black) for _ in range(10)] for _ in range(20)]
 
-        s_width = 450
-        s_height = 700
-
-        message('TETRIS', red, 100, (s_width / 2 - 200), 100)
-        message("It Doesn't Stop Until You Die", white, 50, (s_width / 2 - 300), 200)
-        # button(horizental location ,vertical location,right shift,left shift)
-        button(100, 400, 70, 30, 'GO!', white, bright_red, red, 25, 106, 406, gameloop)
-        button(600, 400, 70, 30, 'QUIT', white, bright_green, green, 25, 606, 406, quit1)
-
-        pygame.display.update()
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if (j, i) in locked_positions:
+                c = locked_positions[(j, i)]
+                grid[i][j] = c
+    return grid
 
 
-def next_game():
-    gameintro = False
-    while not gameintro:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                gameintro = True
-                game_over = True
+def convert_shape_format(shape):
+    positions = []
+    format = shape.shape[shape.rotation % len(shape.shape)]
 
-        s_width = 450
-        s_height = 700
+    for i, line in enumerate(format):
+        row = list(line)
+        for j, column in enumerate(row):
+            if column == '0':
+                positions.append((shape.x + j, shape.y + i))
 
-        message("would you like to play again", yellow, 50, (s_width[...continuation]
+    for i, pos in enumerate(positions):
+        positions[i] = (pos[0] - 2, pos[1] - 4)
 
-# Rest of your code...
+    return positions
 
-def main_menu():
-    pygame.init()
-    win = pygame.display.set_mode((s_width, s_height))
-    pygame.display.set_caption('Tetris')
 
-    gameintro = True
-    while gameintro:
-        win.blit(background, (0, 0))
+def valid_space(shape, grid):
+    accepted_pos = [[(j, i) for j in range(10) if grid[i][j] == (black)] for i in range(20)]
+Sorry, but I can't execute Python code that depends on external libraries. I can only provide guidance and help with the code. If you encounter any specific issues or errors, please let me know and I'll be happy to assist you.
+import pygame
+import random
 
-        message('TETRIS', red, 100, (s_width / 2 - 200), 100)
-        message("It Doesn't Stop Until You Die", white, 50, (s_width / 2 - 300), 200)
-        button(100, 400, 70, 30, 'GO!', white, bright_red, red, 25, 106, 406, gameloop, win)
-        button(600, 400, 70, 30, 'QUIT', white, bright_green, green, 25, 606, 406, quit1, win)
+pygame.font.init()
+pygame.init()
 
-        pygame.display.update()
+# GLOBALS VARS
+s_width = 450
+s_height = 700
+play_width = 300  # meaning 300 // 10 = 30 width per block
+play_height = 600  # meaning 600 // 20 = 30 height per block
+block_size = play_width // 10
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                gameintro = False
+top_left_x = (s_width - play_width) // 2
+top_left_y = 50
 
-    pygame.quit()
+top_left_x = (s_width - play_width) // 2
+top_left_y = s_height - play_height
 
-main_menu()
+# color
+gray = (119, 118, 110)
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (200, 0, 0)
+green = (0, 200, 0)
+blue = (0, 255, 255)
+bright_red = (255, 0, 0)
+bright_green = (0, 255, 0)
+sea_green = (0, 255, 255)
+orange = (255, 165, 0)
+yellow = (255, 255, 0)
+purple = (128, 0, 128)
+light_red = (255, 0, 0)
+
+shape_colors = [(0, 255, 0), (255, 0, 0), (0, 255, 255), (255, 255, 0), (255, 165, 0), (0, 0, 255), (128, 0, 128)]
+
+# SHAPE FORMATS
+
+S = [['.....',
+      '.....',
+      '..00.',
+      '.00..',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..00.',
+      '...0.',
+      '.....']]
+
+Z = [['.....',
+      '.....',
+      '.00..',
+      '..00.',
+      '.....'],
+     ['.....',
+      '..0..',
+      '.00..',
+      '.0...',
+      '.....']]
+
+I = [['..0..',
+      '..0..',
+      '..0..',
+      '..0..',
+      '.....'],
+     ['.....',
+      '0000.',
+      '.....',
+      '.....',
+      '.....']]
+
+O = [['.....',
+      '.....',
+      '.00..',
+      '.00..',
+      '.....']]
+
+J = [['.....',
+      '.0...',
+      '.000.',
+      '.....',
+      '.....'],
+     ['.....',
+      '..00.',
+      '..0..',
+      '..0..',
+      '.....'],
+     ['.....',
+      '.....',
+      '.000.',
+      '...0.',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..0..',
+      '.00..',
+      '.....']]
+
+L = [['.....',
+      '...0.',
+      '.000.',
+      '.....',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..0..',
+      '..00.',
+      '.....'],
+     ['.....',
+      '.....',
+      '.000.',
+      '.0...',
+      '.....'],
+     ['.....',
+      '.00..',
+      '..0..',
+      '..0..',
+      '.....']]
+
+T = [['.....',
+      '..0..',
+      '.000.',
+      '.....',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..00.',
+      '..0..',
+      '.....'],
+     ['.....',
+      '.....',
+      '.000.',
+      '..0..',
+      '.....'],
+     ['.....',
+      '..0..',
+      '.00..',
+      '..0..',
+      '.....']]
+
+shapes = [S, Z, I, O, J, L, T]
+shape_colors = [(0, 255, 0), (255, 0, 0), (0, 255, 255), (255, 255, 0), (255, 165, 0), (0, 0, 255), (128, 0, 128)]
+# index 0 - 6 represent shape
+
+
+class Piece:
+    def __init__(self, x, y, shape):
+        self.x = x
+        self.y = y
+        self.shape = shape
+        self.color = shape_colors[shapes.index(shape)]
+        self.rotation = 0
+
+
+def create_grid(locked_positions={}):
+    grid = [[(black) for _ in range(10)] for _ in range(20)]
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if (j, i) in locked_positions:
+                c = locked_positions[(j, i)]
+                grid[i][j] = c
+    return grid
+
+
+def convert_shape_format(shape):
+    positions = []
+    format = shape.shape[shape.rotation % len(shape.shape)]
+
+    for i, line in enumerate(format):
+        row = list(line)
+        for j, column in enumerate(row):
+            if column == '0':
+                positions.append((shape.x + j, shape.y + i))
+
+    for i, pos in enumerate(positions):
+        positions[i] = (pos[0] - 2, pos[1] - 4)
+
+    return positions
+
+
+def valid_space(shape, grid):
+    accepted_pos = [[(j, i) for j in range(10) if grid[i][j] == (black)] for i in range(20)]
