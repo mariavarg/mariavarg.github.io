99f4dd6a1229bfffc6c762d3e8caefb40b9fd9b6
mariavarg <75952007+mariavarg@users.noreply.github.com>
2023-07-10 22:36:50 +0300

    Update game.js

--- Druna-s-Tetris/game.js
+++ Druna-s-Tetris/game.js
@@ -298,6 +298,7 @@ if __name__ == '__main__':
   
         // Initialize the game
         pygame.init()
-        // ...
+        const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
     `);
 });
