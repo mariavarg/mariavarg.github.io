3719cd42ec2d7f0a26b1f15ff030eb1aa5f1b5b8
mariavarg <75952007+mariavarg@users.noreply.github.com>
2023-07-14 14:44:04 +0300

    Update server.py

--- Druna-s-Tetris/server.py
+++ Druna-s-Tetris/server.py
@@ -4,55 +4,32 @@ import socketserver
 class MyHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/start_server':
-            # Start the Python server
-            try:
-                with socketserver.TCPServer(("", 8000), MyHandler) as httpd:
-                    print("Server started on port 8000")
-                    httpd.serve_forever()
-            except OSError as e:
-                print(f"Failed to start the server: {e}")
-                self.send_response(500)
-                self.end_headers()
-                self.wfile.write(bytes(f"Failed to start the server: {e}", "utf-8"))
-                return
+            # Start the game server
+            start_game()
 
-        # Handle other requests if needed
+            # Send a response to indicate the server has started successfully
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'Server started successfully!')
+
+        # Serve the game files
         else:
             super().do_GET()
+
+def start_game():
+    import pygame
+    import random
+
+    pygame.init()
+
+    # Rest of the game logic goes here...
 
 # Start the server
 if __name__ == "__main__":
     with socketserver.TCPServer(("", 8080), MyHandler) as httpd:
         print("Server started on port 8080")
         httpd.serve_forever()
-import http.server
-import socketserver
-
-class MyHandler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/start_server':
-            # Start the Python server
-            try:
-                with socketserver.TCPServer(("", 8000), MyHandler) as httpd:
-                    print("Server started on port 8000")
-                    httpd.serve_forever()
-            except OSError as e:
-                print(f"Failed to start the server: {e}")
-                self.send_response(500)
-                self.end_headers()
-                self.wfile.write(bytes(f"Failed to start the server: {e}", "utf-8"))
-                return
-
-        # Handle other requests if needed
-        else:
-            super().do_GET()
-
-# Start the server
-if __name__ == "__main__":
-    with socketserver.TCPServer(("", 8080), MyHandler) as httpd:
-        print("Server started on port 8080")
-        httpd.serve_forever()
-
 
 
      
