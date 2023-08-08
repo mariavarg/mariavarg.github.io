5bc5b3dd2f71fb838917fa86c3243580d29a210a
mariavarg <75952007+mariavarg@users.noreply.github.com>
2023-08-08 21:20:45 +0300

    Update server.py

--- Druna-s-Tetris/server.py
+++ Druna-s-Tetris/server.py
@@ -5,40 +5,24 @@ import subprocess
 import json
 
 class MyHandler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/':
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            with open('index.html', 'rb') as file:
-                self.wfile.write(file.read())
-        else:
-            self.send_response(404)
-            super().do_GET()
-
     def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length).decode('utf-8')
-        post_data = json.loads(post_data)
-
-        if 'action' in post_data and post_data['action'] == 'start_server':
+        if self.path == '/start_server':
+            # Start the game server in a separate process
             subprocess.Popen(['python', 'main.py'])  # Use 'python' instead of 'py' for Windows
 
+            # Send a response to indicate the server has started successfully
             self.send_response(200)
-            self.send_header('Content-type', 'application/json')
-            self.send_header('Access-Control-Allow-Origin', '*')
+            self.send_header('Content-type', 'text/plain')
+            self.send_header('Access-Control-Allow-Origin', '*')  # Add the CORS header here
             self.end_headers()
-            response = {'message': 'Server started successfully!'}
-            self.wfile.write(json.dumps(response).encode('utf-8'))
-
-            # Optionally, open the game client in a web browser
-            webbrowser.open('http://localhost:8443')
+            self.wfile.write(b'Server started successfully!')
 
         else:
-            self.send_response(400)
-            self.send_header('Access-Control-Allow-Origin', '*')
+            self.send_response(404)
+            self.send_header('Access-Control-Allow-Origin', '*')  # Add the CORS header here
             self.end_headers()
 
+    
 # Start the HTTP server
 with socketserver.TCPServer(("", 8443), MyHandler) as httpd:  # Listen on port 8443
     print("Server started on port 8443")
